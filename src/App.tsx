ee85a09127886ff734930a321f055be2232e8bee
hinsxd <hins.xd@hotmail.com>
2019-12-13 16:27:10 +0800

    useBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useBoard } from './useBoard';
+import { Box, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  wrapper: {
+    height: '100vh',
+    width: '100vw',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  board: {
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  tile: {
+    height: 60,
+    width: 60,
+    border: '1px solid black',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  const styles = useStyles();
+  const [size, setSize] = useState(4);
+
+  const { board, onTilePress } = useBoard({ size });
+
+  console.log(board);
+  return (
+    <div className={styles.wrapper}>
+      <input
+        type="number"
+        value={size}
+        onChange={e => setSize(parseInt(e.target.value))}
+      />
+      <div className={styles.board}>
+        {board.map((row, i) => (
+          <div className={styles.row} key={`row-${i}`}>
+            {row.map((cell, j) => (
+              <div
+                className={styles.tile}
+                key={`col-${j}`}
+                onClick={() => onTilePress(i, j)}
+              >
+                {cell}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/useBoard.tsx
@@ -0,0 +1,80 @@
+import { Reducer, useReducer, useRef, useEffect } from 'react';
+import produce from 'immer';
+export type Board = number[][];
+
+type useBoardParam = { size?: number };
+type coord = { row: number; col: number };
+type Action =
+  | { type: 'INIT_BOARD'; payload: { size: number } }
+  | { type: 'SWAP'; payload: { a: coord; b: coord } };
+
+const boardReducer: Reducer<Board, Action> = (state, action) => {
+  switch (action.type) {
+    case 'INIT_BOARD':
+      return makeBoard(action.payload.size);
+    case 'SWAP':
+      const { a, b } = action.payload;
+      return produce(state, board => {
+        const temp = board[a.row][a.col];
+        board[a.row][a.col] = board[b.row][b.col];
+        board[b.row][b.col] = temp;
+      });
+    default:
+      return state;
+  }
+};
+
+const makeBoard = (size: number): Board => {
+  let board: Board = [];
+  let count = 1;
+  for (let i = 0; i < size; i++) {
+    board[i] = [];
+    for (let j = 0; j < size; j++, count++) {
+      if (count < size * size) {
+        board[i][j] = count;
+      } else {
+        board[i][j] = 0;
+      }
+    }
+  }
+  return board;
+};
+
+export const useBoard = ({ size = 4 }: useBoardParam) => {
+  const emptyTileCoord = useRef({ row: size - 1, col: size - 1 });
+  const [board, dispatch] = useReducer(boardReducer, makeBoard(size));
+
+  useEffect(() => {
+    dispatch({ type: 'INIT_BOARD', payload: { size } });
+  }, [size]);
+  useEffect(() => {
+    console.log(board);
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[0].length; j++) {
+        if (board[i][j] === 0) {
+          emptyTileCoord.current = { row: i, col: j };
+          console.log({ row: i, col: j });
+        }
+      }
+    }
+  });
+
+  const onTilePress = (row: number, col: number) => {
+    const {
+      current: { row: zeroRow, col: zeroCol }
+    } = emptyTileCoord;
+    const isBesideEmpty =
+      (Math.abs(row - zeroRow) === 1) !== (Math.abs(col - zeroCol) === 1);
+    if (isBesideEmpty) {
+      dispatch({
+        type: 'SWAP',
+        payload: {
+          a: { row, col },
+          b: emptyTileCoord.current
+        }
+      });
+    }
+  };
+
+  return { board, onTilePress };
+};
